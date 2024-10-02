ac01f726aaa7b68aacf571f04442ba1f79a3d949
Deepak Garg <127671311+Deepak-Garg1136@users.noreply.github.com>
2024-10-02 21:41:51 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // vite.config.js
 export default {
   server: {
-    host: '0.0.0.0', // Bind to all addresses
+    host: "0.0.0.0", // Bind to all addresses
     port: 5173, // Specify your desired port (if needed)
   },
 };
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export defineConfig({
+//   plugins: [react()],
+// })
